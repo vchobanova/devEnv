9f77f365cee1a6874c482618e436fda299f53fa6
Vasilena Chobanova <vasilenach@hotmail.com>
2018-05-24 14:38:53 +0200

    home page added

--- server.js
+++ server.js
@@ -50,23 +50,22 @@ app.get("/", (req, res) => {
     res.send( sTopHtml + sMainHtml + sBottomHtml );
     res.end();
 });
-/***********************Views***********************/
-app.get("/", (req, res) => {
+
+app.get("/home", (req, res) => {
     var sTopHtml = fs.readFileSync( __dirname + '/public/components/top.html', 'utf8' );
-    var sMainHtml = fs.readFileSync( __dirname + '/views/login.html', 'utf8' );
+    var sNavHtml = fs.readFileSync( __dirname + '/public/components/nav.html', 'utf8' );
+    var sMainHtml = fs.readFileSync( __dirname + '/views/home.html', 'utf8' );
+    var sFooterHtml = fs.readFileSync( __dirname + '/public/components/footer.html', 'utf8' );
     var sBottomHtml = fs.readFileSync( __dirname + '/public/components/bottom.html', 'utf8' );
 
     //replace placeholders
-    sTopHtml = sTopHtml.replace('{{title}}','Login page');
-
-    sBottomHtml = sBottomHtml.replace('{{customScript}}',  '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>' +
-        '<script src="../public/javascript/login.js"></script>' +
-        '<script src="../public/javascript/register.js"></script>');
-    res.send( sTopHtml + sMainHtml + sBottomHtml );
+    sTopHtml = sTopHtml.replace('{{title}}','Home page');
+    sTopHtml = sTopHtml.replace('{{active-home}}',' active');
+    sTopHtml = sTopHtml.replace(/{{active-.*}}/g ,'');
+    sBottomHtml = sBottomHtml.replace('{{customScript}}',  '<script src="../public/javascript/home.js"></script>');
+    res.send( sTopHtml + sNavHtml + sMainHtml + sFooterHtml + sBottomHtml );
     res.end();
 });
-
-
 
 app.listen(8080, (err) => {
     if(err) return err;
