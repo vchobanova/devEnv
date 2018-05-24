7e42f5f87f7c14e2bfbcb7738998d397ccf8417f
Vasilena Chobanova <vasilenach@hotmail.com>
2018-05-24 14:12:08 +0200

    setting up the views

--- server.js
+++ server.js
@@ -32,7 +32,40 @@ const user = require(__dirname + "/routes/user.js");
 app.use("/account", user);
 /****************************************************/
 
+/***********************Views***********************/
+app.get("/", (req, res) => {
+    var sTopHtml = fs.readFileSync( __dirname + '/public/components/top.html', 'utf8' );
+    var sMainHtml = fs.readFileSync( __dirname + '/views/index.html', 'utf8' );
+    var sBottomHtml = fs.readFileSync( __dirname + '/public/components/bottom.html', 'utf8' );
 
+    //replace placeholders
+    sTopHtml = sTopHtml.replace('{{title}}','Web shop home page');
+    sTopHtml = sTopHtml.replace('{{active-home}}',' active');
+    sTopHtml = sTopHtml.replace(/{{active-.*}}/g ,'');
+    sBottomHtml = sBottomHtml.replace('{{customScript}}',  '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>' +
+        '<script src="../public/javascript/general.js"></script>' +
+        '<script src="../public/javascript/login.js"></script>' +
+        '<script src="../public/javascript/logout.js"></script>' +
+        '<script src="../public/javascript/register.js"></script>' +
+        '<script src="../public/javascript/homePage.js"></script>');
+    res.send( sTopHtml + sMainHtml + sBottomHtml );
+    res.end();
+});
+/***********************Views***********************/
+app.get("/", (req, res) => {
+    var sTopHtml = fs.readFileSync( __dirname + '/public/components/top.html', 'utf8' );
+    var sMainHtml = fs.readFileSync( __dirname + '/views/login.html', 'utf8' );
+    var sBottomHtml = fs.readFileSync( __dirname + '/public/components/bottom.html', 'utf8' );
+
+    //replace placeholders
+    sTopHtml = sTopHtml.replace('{{title}}','Login page');
+
+    sBottomHtml = sBottomHtml.replace('{{customScript}}',  '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>' +
+        '<script src="../public/javascript/login.js"></script>' +
+        '<script src="../public/javascript/register.js"></script>');
+    res.send( sTopHtml + sMainHtml + sBottomHtml );
+    res.end();
+});
 
 
 
